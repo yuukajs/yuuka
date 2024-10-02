14e863c5afd93d5433b61cf683ebc9bf6a255e7e
Lisk <liskqwq@gmail.com>
2024-10-02 10:54:28 +0800

    🎨 chore(format): format code with prettier

--- src/core/flow.ts
+++ src/core/flow.ts
@@ -1,30 +1,34 @@
 export class Flow {
-  solute: any
-  solvent: any
-  defines: {lambda: (x: any) => boolean; body: (ctx: any) => any}[] = []
+  solute: any;
+  solvent: any;
+  defines: Domer<any>[] = [];
   constructor() {
-    this.solute = { value: null }
+    this.solute = { value: null };
     this.solvent = new Proxy(this.solute, {
       set: (old, prop, value) => {
-        this.flowout(old, value)
-        old.prop = value
-        return !0
+        this.flowout(old, value);
+        old.prop = value;
+        return !0;
       },
-    })
+    });
   }
 
   flow(liquid) {
-    this.solvent.value = liquid
+    this.solvent.value = liquid;
   }
 
-  dom(lambda, body) {
-    this.defines.push({ lambda, body })
+  dom<T>(lambda: (e: T) => boolean, body: (e: T) => any) {
+    this.defines.push({ lambda, body });
   }
 
   flowout(old: any, value: any) {
-    const effects = this.defines.filter(e => e.lambda(value))
+    const effects = this.defines.filter((e) => e.lambda(value));
     effects.forEach(({ body }) => {
-      body(value)
-    })
+      body(value);
+    });
   }
+}
+interface Domer<T> {
+  lambda: (x: T) => boolean;
+  body: (x: T) => any;
 }
