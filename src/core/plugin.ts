6fa3f0b72ab5e77c47146e92e13e84307f3b0dc5
Lisk <liskqwq@gmail.com>
2024-11-11 23:41:47 +0800

    ✨ feat: add specific plugin define

--- src/core/plugin.ts
+++ src/core/plugin.ts
@@ -1,20 +1,17 @@
-export function definePlugin<
-  T extends { config: { [key: string]: any }; apply: (...args: any[]) => void },
-  S extends any,
->(plugin: { apply: (ctx: S, config: T["config"]) => void }) {
-  return plugin;
+import { Context, Application } from '@/types'
+
+interface Plugin<T> extends Application<T>{
+  config: T
 }
+export function definePlugin(plugin: Plugin) {
+  return plugin
+}
+
 export class Plugins {
-  plugins: { apply: (...args: any[]) => void }[];
-  ctx: any;
-  constructor(ctx: any) {
-    this.ctx = ctx
-  }
-  register<T extends { [key: string]: any }>(config: T, plugin: {
-    config: T;
-    apply: (ctx: any, config: T) => void;
-  }): boolean {
+  plugins: Plugin<any>[]
+  constructor() {}
+  register<T>(plugin: Plugin<T>): boolean {
     plugin.apply(this.ctx, config)
-    return !!this.plugins.push(plugin);
+    return !!this.plugins.push(plugin)
   }
 }
